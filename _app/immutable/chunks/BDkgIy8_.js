de7cf60077f8698044fcef88dcfdcf8fbeaa174d
hhsdev <heinhtetsoe.dev@gmail.com>
2026-02-15 14:51:31 +0800

    Updates

--- _app/immutable/chunks/BDkgIy8_.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1lujksb?.base??"/sql-visualizer",a=globalThis.__sveltekit_1lujksb?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/DMUlMtMh.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_15a6c9p?.base??"/sql-visualizer",a=globalThis.__sveltekit_15a6c9p?.assets??s??"";export{a,s as b};
